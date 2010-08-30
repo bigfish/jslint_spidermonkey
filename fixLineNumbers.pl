eb5d1e856fa54cc43232eac68848a1cfbc42ee7d
David Wilhelm <david@jupiter.(none)>
2010-08-30 14:54:20 -0400

    added fix for line numbers with options

---
+++ fixLineNumbers.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+#correct(reduce) the line numbers given by lint using the given argument 
+# - which is the number of lines in the options file used by jslint script
+
+$decrement = shift;
+while(<STDIN>)
+{
+	my($line) = $_;
+	chomp($line);
+	if($line =~ /Lint at line (\d*)/g ){
+			$line_num = $1;
+			$line_num = $line_num - $decrement;
+			$line =~ s/Lint at line (\d*)/Lint at line $line_num/g;
+	}
+	print $line."\n";
+}
+
